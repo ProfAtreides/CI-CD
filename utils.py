1649185f82d0b16f84e54133fa61ba1b27b9fd36
ProfAtreides <dambrozewicz7@gmail.com>
2024-05-28 18:06:16 +0200

    Added formating

--- utils.py
+++ utils.py
@@ -1,20 +1,21 @@
 """"Module providing basic math operations"""
 
+
 def add(a: int, b: int) -> int:
-    """"Adding two numbers together"""
+    """ "Adding two numbers together"""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
-    """"Subtracting two numbers together"""
+    """ "Subtracting two numbers together"""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
-    """"Multiplying two numbers together"""
+    """ "Multiplying two numbers together"""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
-    """"Dividing two numbers together"""
+    """ "Dividing two numbers together"""
     return a / b
